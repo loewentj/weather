e62c229f3796debc971bc6c702933fdcb9c6c2a6
loewentj <38169321+loewentj@users.noreply.github.com>
2020-01-09 16:37:53 +1100

    Update

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ cityForm.addEventListener('submit', e => {
   // get city value
   const city = cityForm.city.value.trim();
   cityForm.reset();
-  cityForm.blur();
+  cityForm.input.blur();
 
   // update the ui with new city
   updateCity(city)
